197634c9f9cf6be871cc5359430e847d2cc4189d
OpportunityLiu <opportunity@live.in>
2019-08-09 17:24:44 +0800

    检查FF移动版不支持的API

--- src/background.ts
+++ src/background.ts
@@ -9,8 +9,8 @@ var lastCheck = 0;
 var lastCheckData: any;
 var TagList: TagList = [];
 var loadLock = false;
-async function download (): Promise<EHTDatabase>{
-  return new Promise<EHTDatabase>(async (resolve, reject) =>  {
+async function download(): Promise<EHTDatabase> {
+  return new Promise<EHTDatabase>(async (resolve, reject) => {
     if (loadLock) {
       return false;
     }
@@ -18,7 +18,7 @@ async function download (): Promise<EHTDatabase>{
 
     const githubDownloadUrl = 'https://api.github.com/repos/ehtagtranslation/Database/releases/latest';
     const info = await (await fetch(githubDownloadUrl)).json();
-    const asset = info.assets.find((i:any) => i.name === 'db.raw.json.gz');
+    const asset = info.assets.find((i: any) => i.name === 'db.raw.json.gz');
     const url = asset && asset.browser_download_url || '';
     if (!url) {
       console.error('地址未找到', url);
@@ -30,12 +30,12 @@ async function download (): Promise<EHTDatabase>{
     var xhr = new XMLHttpRequest()
     xhr.open('GET', url)
     xhr.responseType = "arraybuffer";
-    xhr.onload = function() {
+    xhr.onload = function () {
       loadLock = false;
       try {
-         const data = JSON.parse(pako.ungzip(xhr.response, {to: "string"}));
-         resolve(data as EHTDatabase);
-         badge.set('100', "#4A90E2", 1);
+        const data = JSON.parse(pako.ungzip(xhr.response, { to: "string" }));
+        resolve(data as EHTDatabase);
+        badge.set('100', "#4A90E2", 1);
       } catch (e) {
         reject();
         badge.set('Err', "#C80000");
@@ -77,28 +77,28 @@ chromeMessage.listener('check-version', (data, callback) => {
 chrome.storage.local.get((data) => {
   if (!('tagList' in data && 'tagReplaceData' in data)) {
     const dbUrl = chrome.runtime.getURL('assets/tag.db');
-    fetch(dbUrl).then(r => r.text()).then(text=> {
+    fetch(dbUrl).then(r => r.text()).then(text => {
       const data = JSON.parse(text);
       storageTagData(data)
     })
   }
-  if('tagList' in data){
+  if ('tagList' in data) {
     TagList = data.tagList;
   }
 })
 
 function storageTagData(tagDB: EHTDatabase): Promise<any> {
   return new Promise(resolve => {
-    const namespaceOrder = ['female','language','misc', 'male','artist', 'group', 'parody', 'character', 'reclass'];
-    const tagReplaceData: {[key: string]: string} = {};
+    const namespaceOrder = ['female', 'language', 'misc', 'male', 'artist', 'group', 'parody', 'character', 'reclass'];
+    const tagReplaceData: { [key: string]: string } = {};
     const tagList: TagList = [];
-    tagDB.data.sort((a,b) => {
+    tagDB.data.sort((a, b) => {
       return namespaceOrder.indexOf(a.namespace) - namespaceOrder.indexOf(b.namespace);
     })
     tagDB.data.forEach(space => {
       const namespace = space.namespace;
-      if(namespace === 'rows') return;
-      for(let key in space.data){
+      if (namespace === 'rows') return;
+      for (let key in space.data) {
         const t = space.data[key];
         let search = ``;
         if (namespace !== 'misc') {
@@ -130,7 +130,7 @@ function storageTagData(tagDB: EHTDatabase): Promise<any> {
       tagReplaceData,
       updateTime: new Date().getTime(),
       sha: tagDB.head.sha,
-    },() => {
+    }, () => {
       resolve();
       badge.set('OK', "#00C801");
       setTimeout(() => {
@@ -141,60 +141,64 @@ function storageTagData(tagDB: EHTDatabase): Promise<any> {
   })
 }
 
-chrome.contextMenus.create(
-  {
-    documentUrlPatterns: ["*://exhentai.org/*", "*://e-hentai.org/*", "*://*.exhentai.org/*", "*://*.e-hentai.org/*"],
-    title: '提交标签翻译',
-    targetUrlPatterns: ["*://exhentai.org/tag/*", "*://e-hentai.org/tag/*", "*://*.exhentai.org/tag/*", "*://*.e-hentai.org/tag/*"],
-    contexts: ['link'],
-    onclick(info, tab) {
-      if(/\/tag\//.test(info.linkUrl)){
-        const s = info.linkUrl.replace('+', ' ').split('/')
-        const s2 = s[s.length - 1].split(':')
-        const namespace = s2.length == 1 ? 'misc' : s2[0];
-        const tag = s2.length == 1 ? s2[0] : s2[1];
-        const editorUlr = `https://ehtagtranslation.github.io/Editor/edit/${encodeURIComponent(namespace)}/${encodeURIComponent(tag)}`;
-        chrome.tabs.create({
-          url: editorUlr,
-        })
+if (chrome.contextMenus) {
+  chrome.contextMenus.create(
+    {
+      documentUrlPatterns: ["*://exhentai.org/*", "*://e-hentai.org/*", "*://*.exhentai.org/*", "*://*.e-hentai.org/*"],
+      title: '提交标签翻译',
+      targetUrlPatterns: ["*://exhentai.org/tag/*", "*://e-hentai.org/tag/*", "*://*.exhentai.org/tag/*", "*://*.e-hentai.org/tag/*"],
+      contexts: ['link'],
+      onclick(info, tab) {
+        if (/\/tag\//.test(info.linkUrl)) {
+          const s = info.linkUrl.replace('+', ' ').split('/')
+          const s2 = s[s.length - 1].split(':')
+          const namespace = s2.length == 1 ? 'misc' : s2[0];
+          const tag = s2.length == 1 ? s2[0] : s2[1];
+          const editorUlr = `https://ehtagtranslation.github.io/Editor/edit/${encodeURIComponent(namespace)}/${encodeURIComponent(tag)}`;
+          chrome.tabs.create({
+            url: editorUlr,
+          })
+        }
       }
+    }, () => {
     }
-  }, () => {
-  }
-)
+  )
+}
 
-chrome.omnibox.onInputChanged.addListener(
-  function(text, suggest) {
-    if(TagList.length){
-      const data = TagList.filter(v => v.search.indexOf(text) !== -1 || v.name.indexOf(text) !== -1).slice(0, 5).map(tag => {
-        const cnNamespace = namespaceTranslate[tag.namespace];
-        let cnNameHtml = '';
-        let enNameHtml = tag.search;
-        if (tag.namespace !== 'misc') {
-          cnNameHtml += cnNamespace + ':';
-        }
-        cnNameHtml += tag.name;
-        return {
-          content: enNameHtml,
-          description: cnNameHtml,
-        }
-      })
-      suggest(data);
-    }
-  });
+if (chrome.omnibox) {
+  chrome.omnibox.onInputChanged.addListener(
+    function (text, suggest) {
+      if (TagList.length) {
+        const data = TagList.filter(v => v.search.indexOf(text) !== -1 || v.name.indexOf(text) !== -1).slice(0, 5).map(tag => {
+          const cnNamespace = namespaceTranslate[tag.namespace];
+          let cnNameHtml = '';
+          let enNameHtml = tag.search;
+          if (tag.namespace !== 'misc') {
+            cnNameHtml += cnNamespace + ':';
+          }
+          cnNameHtml += tag.name;
+          return {
+            content: enNameHtml,
+            description: cnNameHtml,
+          }
+        })
+        suggest(data);
+      }
+    });
 
-chrome.omnibox.onInputEntered.addListener(
-  function(text) {
-    chrome.tabs.create({
-      url: `https://e-hentai.org/?f_search=${encodeURIComponent(text)}`,
-    })
-  });
+  chrome.omnibox.onInputEntered.addListener(
+    function (text) {
+      chrome.tabs.create({
+        url: `https://e-hentai.org/?f_search=${encodeURIComponent(text)}`,
+      });
+    });
+}
 
 async function checkVersion() {
   const time = new Date().getTime();
   // 限制每分钟最多请求1次
   const { sha } = await new Promise((resolve) => chrome.storage.local.get(resolve));
-  if((time - lastCheck <= 1000 * 60) && lastCheckData) {
+  if ((time - lastCheck <= 1000 * 60) && lastCheckData) {
     return {
       new: (lastCheckData && lastCheckData.new) ? lastCheckData.new : '',
       old: sha,
@@ -204,7 +208,7 @@ async function checkVersion() {
   const githubDownloadUrl = 'https://api.github.com/repos/ehtagtranslation/Database/releases/latest';
   const info = await (await fetch(githubDownloadUrl)).json();
 
-  if(!(info && info.target_commitish)){
+  if (!(info && info.target_commitish)) {
     return null;
   }
   lastCheckData = {
--- src/tool/badge-loading.ts
+++ src/tool/badge-loading.ts
@@ -1,4 +1,11 @@
 export class BadgeLoading {
+
+  constructor() {
+    chrome.management.getSelf(data => {
+      this.extname = data.name || this.extname;
+    });
+  }
+
   loadingStrArr = [
     [''],
     '⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏'.split(''),
@@ -8,40 +15,56 @@ export class BadgeLoading {
 
   frame = 0;
   index = 0;
-  interval = 0;
+  interval?: ReturnType<typeof setTimeout> = undefined;
   text = '';
   loadingString: string[] = [''];
   color = '';
 
-  set(text: string, color: string = '', loading = 0) {
-    if(this.index != loading) {
+  extname = 'EhSyringe';
+
+  private setColor(color: string): void {
+    if (this.color !== color) {
+      this.color = color;
+      if (chrome.browserAction.setBadgeBackgroundColor) {
+        chrome.browserAction.setBadgeBackgroundColor({ color });
+      }
+    }
+  }
+
+  private setText(text: string): void {
+    if (chrome.browserAction.setBadgeText) {
+      chrome.browserAction.setBadgeText({ text });
+    } else {
+      const title = text ? `${this.extname} (${text})` : this.extname;
+      chrome.browserAction.setTitle({ title });
+    }
+  }
+
+  set(text: string, color: string = '', loading: number = 0): void {
+    if (this.index !== loading) {
       this.index = loading;
       this.loadingString = this.loadingStrArr[this.index] || [''];
       this.frame = 0;
     }
     this.text = text;
-    if (this.color != color) {
-      this.color = color;
-      chrome.browserAction.setBadgeBackgroundColor({color});
-    }
-    if(loading){
-      if(!this.interval){
+    this.setColor(color);
+    if (loading) {
+      if (!this.interval) {
         this.interval = setInterval(() => {
-          const text = this.text + (this.loadingString[this.frame] || '');
-          chrome.browserAction.setBadgeText({text});
-          this.frame ++;
-          if(!this.loadingString[this.frame]){
+          this.setText(`${this.text}${this.loadingString[this.frame] || ''}`);
+          this.frame++;
+          if (!this.loadingString[this.frame]) {
             this.frame = 0;
           }
         }, 100);
       }
     } else {
       this.frame = 0;
-      if(this.interval){
+      if (this.interval) {
         clearInterval(this.interval);
-        this.interval = 0;
+        this.interval = undefined;
       }
-      chrome.browserAction.setBadgeText({text: this.text})
+      this.setText(this.text);
     }
   }
 }
