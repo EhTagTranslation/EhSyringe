a20fbe03b9fec19fba7a5fb0de7416176143d89d
xin <i@xioxin.com>
2019-08-13 13:12:26 +0800

    Popup 采用轻量化 VDOM

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,217 +1,263 @@
 import './popup.less';
 import { chromeMessage } from '../tool/chrome-message';
 import { dateDiff } from '../tool/tool';
+import {html, render, svg} from 'lit-html';
 
 function sleep(ms: number): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-function elementBinding<T = any>(selectors: string, attribute?: string) {
-  return function (target: any, name: any) {
-    let element = document.querySelector(selectors) as any;
-    Object.defineProperty(target, name, {
-      get() {
-        return element ? element[attribute] : null;
-      },
-      set(v: T) {
-        if (!element) element = document.querySelector(selectors) as any;
-        if (element) element[attribute] = v;
-      },
-      enumerable: true
-    })
-  }
-}
-
-function element(selectors: string) {
-  return function (target: any, name: any) {
-    let element = document.querySelector(selectors) as any;
-    Object.defineProperty(target, name, {
-      get() {
-        if (!element) element = document.querySelector(selectors) as any;
-        if (element) return element;
-      },
-      enumerable: true
-    })
-  }
-}
-
-function elementListener(selectors: string, eventName: string) {
-  return function (target: any, name: any) {
-    let value = target[name];
-    Object.defineProperty(target, name, {
-      get() {
-        return value
-      },
-      set(v) {
-        value = v;
-        const element = document.querySelector(selectors) as HTMLElement;
-        if (element) {
-          element.addEventListener(eventName, value);
-        }
-      },
-      enumerable: true
-    })
-  }
+interface PopupState {
+  sha: string;
+  updateTime: string;
+  updateTimeFull: string;
+  extensionVersion: string;
+  shaRef: string;
+  newSha: string;
+  versionInfo: string;
+  updateAvailable: boolean;
+  updateButtonDisabled: boolean;
+  showSettingPanel: boolean;
+  progress: number,
+  animationState: number,
+  info: string;
 }
 
 class Popup {
-  private testAnimationIndex: number = 0;
-  private testAnimationList: [string, number][] = [
-    ['prominent', 0],
-    ['prominent', 10],
-    ['prominent', 30],
-    ['prominent', 80],
-    ['prominent', 100],
-    ['prominent injection', 100],
-    ['prominent injection', 5],
-    ['prominent', 5],
-    ['', 0],
-  ];
 
   constructor() {
-
+    this._update();
     this.getVersion();
     this.checkVersion().then();
-
     chrome.management.getSelf(data => {
-      document.title = data.name;
-      this.extensionVersion = `V${data.version}`;
+      this.state.extensionVersion = `${data.version}`;
     });
     chrome.runtime.onMessage.addListener((request) => {
       if ('cmd' in request && request.cmd == 'downloadStatus') {
         this.downloadStatus(request.data).then();
       }
     });
-
   }
 
-  @element('#checkVersion')
-  private checkVersionElement: HTMLElement;
-
-  @element('.logo')
-  private logoElement: HTMLElement;
-
-  @element('#PushRod')
-  private PushRod: SVGGElement;
-
-  @element('#Enema')
-  private Enema: SVGRectElement;
-
-  @element('#settingPanel')
-  private settingPanelElement: HTMLDivElement;
-
-  @element('#updateButton')
-  private updateButtonElement: HTMLButtonElement;
-
-  @elementBinding('#sha', 'textContent')
-  private sha = '';
-
-  @elementBinding('#sha', 'href')
-  private sharef = '';
-
-  @elementBinding('#updateTime', 'textContent')
-  private updateTime = '';
-
-  @elementBinding('#info', 'textContent')
-  private info = '';
-
-  @elementBinding('#extensionVersion', 'textContent')
-  private extensionVersion = '';
-
-  @elementListener('#updateButton', 'click')
-  private updateButtonClick = async () => {
-    this.updateButtonElement.disabled = true;
-    await chromeMessage.send('get-tag-data', {});
-    setTimeout(() => {
-      this.updateButtonElement.disabled = false;
-      this.getVersion();
-      this.checkVersion().then();
-    }, 1000)
+  private _state: PopupState = {
+    sha: '',
+    info: '',
+    updateTime: '',
+    updateTimeFull: '',
+    extensionVersion: '',
+    shaRef: '',
+    newSha: '',
+    versionInfo: '',
+    updateAvailable: false,
+    updateButtonDisabled: false,
+    showSettingPanel: false,
+    progress: 0,
+    animationState: 0,
   };
+  private state: PopupState = new Proxy(this._state, {
+    set:  (target, key, value, receiver) => {
+      const r = Reflect.set(target, key, value, receiver);
+      this._update();
+      return r;
+    }
+  });
 
-  @elementListener('#settingSwitch', 'click')
-  private settingSwitchClick = () => {
-    this.settingPanelElement.classList.toggle('open');
-  };
+  private testAnimationIndex: number = 0;
+  private testAnimationList: [number, number][] = [
+    [1, 0],
+    [1, 10],
+    [1, 30],
+    [1, 80],
+    [1, 100],
+    [2, 100],
+    [2, 5],
+    [1, 5],
+    [0, 0],
+  ];
 
-  // 只是动画测试 可以点着玩
-  @elementListener('.logo', 'click')
-  private logoClick = () => {
+  testAnimation() {
     const a = this.testAnimationList[this.testAnimationIndex];
     this.testAnimationIndex++;
     if (!this.testAnimationList[this.testAnimationIndex]) {
       this.testAnimationIndex = 0;
     }
-    this.logoElement.className = 'logo ' + a[0];
-    this.setProgress(a[1])
-  };
-
-
-  async downloadStatus(data: any) {
-    let className = ['logo'];
-    this.updateButtonElement.disabled = data.run;
-    if (data.run) {
-      className.push('prominent');
-    }
-    this.info = data.info;
-    this.setProgress(data.progress || 0);
-    this.logoElement.className = className.join(' ');
-
-    if (data.complete) {
-
-      await sleep(1000);
-      className.push('injection');
-      this.setProgress(100);
-      this.logoElement.className = className.join(' ');
-
-      await sleep(500);
-      this.setProgress(5);
-
-      await sleep(500);
-      className.pop();
-      this.logoElement.className = className.join(' ');
-    }
-  }
-
-  setProgress(p: number) {
-    const maxWidth = 70;
-    this.PushRod.style.transform = `translate(${((p / 400) * maxWidth).toFixed(2)}px, 0)`;
-    this.Enema.style.transform = `scaleX(${p / 100})`;
-    // this.Enema.setAttribute('width',((p / 100) * maxWidth).toFixed(2));
-  }
-
-
-  async checkVersion() {
-    this.checkVersionElement.textContent = '检查中...';
-    const data = await chromeMessage.send('check-version', {});
-    console.log(data);
-    if (data && data.new) {
-      const hasNewData = data.new !== data.old;
-      if (hasNewData) {
-        this.checkVersionElement.innerHTML = `<a href="${data.newLink}">${data.new.slice(0, 6)}</a> 有更新！`;
-        this.checkVersionElement.classList.add('hasNew');
-        this.updateButtonElement.classList.add('primary');
-        this.updateButtonElement.textContent = '更新';
-      } else {
-        this.checkVersionElement.textContent = '已是最新版本';
-        this.checkVersionElement.classList.remove('hasNew');
-        this.updateButtonElement.classList.remove('primary');
-        this.updateButtonElement.textContent = '更新';
-      }
-    } else {
-      this.checkVersionElement.textContent = '获取失败';
-    }
+    this.state.animationState = a[0];
+    this.state.progress = a[1];
   }
 
   getVersion() {
     chrome.storage.local.get((data) => {
-      this.sha = data.sha ? data.sha.slice(0, 6) : 'N/A';
-      this.sharef = data.releaseLink || '';
-      this.updateTime = data.updateTime ? dateDiff(data.updateTime) : 'N/A';
-      // updateTime.title = new Date(data.updateTime).toLocaleString();
+      this.state.sha = data.sha ? data.sha.slice(0, 6) : 'N/A';
+      this.state.shaRef = data.releaseLink || '';
+      this.state.updateTime = data.updateTime ? dateDiff(data.updateTime) : 'N/A';
+      this.state.updateTimeFull = new Date(data.updateTime).toLocaleString();
     });
   }
 
+  async checkVersion() {
+    this.state.versionInfo = '检查中...';
+    const data = await chromeMessage.send('check-version', {});
+    console.log(data);
+    if (data && data.new) {
+      const hasNewData = this.state.updateAvailable = data.new !== data.old;
+      if (hasNewData) {
+        this.state.newSha = data.new.slice(0, 6);
+        this.state.versionInfo = `有更新！`;
+      } else {
+        this.state.versionInfo = '已是最新版本';
+      }
+    } else {
+      this.state.versionInfo  = '获取失败';
+    }
+  }
+
+
+  async downloadStatus(data: any) {
+    this.state.updateButtonDisabled = data.run;
+    this.state.animationState = data.run ? 1 : 0;
+    this.state.info = data.info;
+    this.state.progress = data.progress || 0;
+    if (data.complete) {
+      await sleep(1000);
+      this.state.progress = 100;
+      this.state.animationState = 2;
+
+      await sleep(500);
+      this.state.progress = 5;
+
+      await sleep(500);
+      this.state.animationState = 1;
+    }
+  }
+
+  private updateButtonClick = async () => {
+    this.state.updateButtonDisabled = true;
+    await chromeMessage.send('get-tag-data', {});
+    setTimeout(() => {
+      this.state.updateButtonDisabled = false;
+      this.getVersion();
+      this.checkVersion().then();
+    }, 1000)
+  };
+
+  _logoTemplate(progress = 0) {
+
+    const PushRodStyle = `transform: translate(${(progress / 400) * 70}px, 0)`;
+    const EnemaStyle = `transform: scaleX(${progress / 100})`;
+
+    return svg`
+    <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
+      <defs>
+          <clipPath id="clip">
+              <rect width="200" height="200"/>
+          </clipPath>
+      </defs>
+      <g clip-path="url(#clip)">
+          <g id="PushRod" style="${PushRodStyle}">
+              <g transform="translate(-39 -312)">
+                  <g transform="translate(131 403)" fill="#fff" stroke="#7836a0" stroke-width="6">
+                      <rect width="78" height="18" rx="6" stroke="none"/>
+                      <rect x="3" y="3" width="72" height="12" rx="3" fill="none"/>
+                  </g>
+                  <g transform="translate(203 391)" fill="#fff" stroke="#7836a0" stroke-width="6">
+                      <rect width="18" height="43" rx="9" stroke="none"/>
+                      <rect x="3" y="3" width="12" height="37" rx="6" fill="none"/>
+                  </g>
+              </g>
+          </g>
+          <g transform="translate(56 85)" fill="#fff" stroke="#7836a0" stroke-width="6">
+              <rect width="83" height="30" rx="6" stroke="none"/>
+              <rect x="3" y="3" width="77" height="24" rx="3" fill="none"/>
+          </g>
+          <g id="Enema" style="${EnemaStyle}">
+              <rect width="70" height="27" transform="translate(61 86)" fill="#fdbdf1"/>
+          </g>
+          <path id="Enema2" d="M27.426,86.36s5.65.007,13.6.006S57.64,83.357,57.64,83.357L38.015,102.982,24.409,89.377l3.015-3.015Z" transform="translate(107.906 10.036) rotate(45)" fill="#fdbdf1"/>
+          <g transform="translate(-39 -312)">
+              <g transform="translate(95 397)" fill="none" stroke="#7836a0" stroke-width="6">
+                  <rect width="83" height="30" rx="6" stroke="none"/>
+                  <rect x="3" y="3" width="77" height="24" rx="3" fill="none"/>
+              </g>
+              <g transform="translate(82 403)" fill="#fdbdf1" stroke="#7836a0" stroke-width="6">
+                  <rect width="19" height="18" rx="6" stroke="none"/>
+                  <rect x="3" y="3" width="13" height="12" rx="3" fill="none"/>
+              </g>
+              <rect width="29" height="6" rx="3" transform="translate(58 409)" fill="#7836a0"/>
+              <g transform="translate(172 381)" fill="#fff" stroke="#7836a0" stroke-width="6">
+                  <rect width="18" height="62" rx="9" stroke="none"/>
+                  <rect x="3" y="3" width="12" height="56" rx="6" fill="none"/>
+              </g>
+              <g transform="translate(119 398)" fill="#fff" stroke="#7836a0" stroke-width="3">
+                  <rect width="4" height="11" rx="2" stroke="none"/>
+                  <rect x="1.5" y="1.5" width="1" height="8" rx="0.5" fill="none"/>
+              </g>
+              <g transform="translate(131 398)" fill="#fff" stroke="#7836a0" stroke-width="3">
+                  <rect width="4" height="11" rx="2" stroke="none"/>
+                  <rect x="1.5" y="1.5" width="1" height="8" rx="0.5" fill="none"/>
+              </g>
+              <g transform="translate(144 398)" fill="#fff" stroke="#7836a0" stroke-width="3">
+                  <rect width="4" height="11" rx="2" stroke="none"/>
+                  <rect x="1.5" y="1.5" width="1" height="8" rx="0.5" fill="none"/>
+              </g>
+              <g transform="translate(156 398)" fill="#fff" stroke="#7836a0" stroke-width="3">
+                  <rect width="4" height="11" rx="2" stroke="none"/>
+                  <rect x="1.5" y="1.5" width="1" height="8" rx="0.5" fill="none"/>
+              </g>
+          </g>
+      </g>
+  </svg>`;
+  }
+
+  _template() {
+    const state = this.state;
+    return html`
+<div style="max-width: 400px; min-width: 200px; margin: auto;">
+    <div class="logo-box" style="height: 205px;">
+        <div
+        class="logo ${['', 'prominent', 'prominent injection'][state.animationState] || ''}"
+        @click="${() => {this.testAnimation()}}">
+            ${this._logoTemplate(state.progress)}
+        </div>
+        <div id="info">${state.info}</div>
+    </div>
+    <table>
+        <tr>
+            <th class="no-select">TAG版本：</th>
+            <td><a id="sha" class="monospace">${state.sha}</a></td>
+        </tr>
+        <tr>
+            <th class="no-select">上次更新：</th>
+            <td><span id="updateTime" class="monospace">${state.updateTime}</span></td>
+        </tr>
+        <tr>
+            <th class="no-select">更新检查：</th>
+            <td><span id="checkVersion" class="monospace ${state.updateAvailable ? 'hasNew' : ''}"><a>${state.newSha}</a></span> ${state.versionInfo}</td>
+        </tr>
+    </table>
+    <button @click="${() => this.updateButtonClick()}" ?disabled=${state.updateButtonDisabled} class="big-button ${state.updateAvailable ? 'primary' : ''}" id="updateButton">更新</button>
+    <div class="foot-buttons">
+        <div>
+            <a id="settingSwitch" href="#" @click="${() => state.showSettingPanel = !state.showSettingPanel}">设置</a>
+        </div>
+        <div class="cushion"></div>
+        <div>
+            <span id="extensionVersion" class="monospace">V${state.extensionVersion}</span>
+        </div>
+    </div>
+</div>
+<div id="settingPanel" class="${ state.showSettingPanel ? 'open' : ''}" >
+    <div class="form">
+        <form id="settingForm">
+            <input type="text" name="test">
+        </form>
+    </div>
+    <button class="big-button primary">保存</button>
+</div>`;
+  }
+
+  _update() {
+    render(this._template(), document.body);
+  }
 }
 
 window.onload = () => new Popup();
