1b4582fe998162fef41889eea95986527302ee55
OpportunityLiu <opportunity@live.in>
2019-09-20 17:37:15 +0800

    Revert "移除 OmniBox"
    This reverts commit d4398ea3878f0ad554476c34201181a7a49f3165.

--- src/background/omnibox.ts
+++ src/background/omnibox.ts
@@ -4,62 +4,61 @@ import { makeTagMatchHtml } from '../tool/tool';
 
 import { suggest } from './suggest';
 
-// class OmniBox {
-//     constructor() {
-//         chrome.omnibox.onInputChanged.addListener(this.onInputChanged);
-//         chrome.omnibox.onInputEntered.addListener(this.onInputEntered);
-//         chrome.omnibox.onInputStarted.addListener(this.onInputStarted);
-//     }
+class OmniBox {
+    constructor() {
+        chrome.omnibox.onInputChanged.addListener(this.onInputChanged);
+        chrome.omnibox.onInputEntered.addListener(this.onInputEntered);
+        chrome.omnibox.onInputStarted.addListener(this.onInputStarted);
+    }
 
-//     origin: string = 'https://e-hentai.org';
+    origin: string = 'https://e-hentai.org';
 
-//     readonly onInputStarted = async () => {
-//         const tabs = await browser.tabs.query({
-//             url: [
-//                 '*://exhentai.org/*',
-//                 '*://e-hentai.org/*',
-//             ]
-//         });
-//         const tab = tabs.find(t => t.active) || tabs[0];
-//         if (tab) {
-//             this.origin = new URL(tab.url).origin;
-//         } else {
-//             this.origin = 'https://e-hentai.org';
-//         }
-//     }
+    readonly onInputStarted = async () => {
+        const tabs = await browser.tabs.query({
+            url: [
+                '*://exhentai.org/*',
+                '*://e-hentai.org/*',
+            ]
+        });
+        const tab = tabs.find(t => t.active) || tabs[0];
+        if (tab) {
+            this.origin = new URL(tab.url).origin;
+        } else {
+            this.origin = 'https://e-hentai.org';
+        }
+    }
 
-//     readonly onInputChanged = async (text: string, suggestCb: (suggestResults: chrome.omnibox.SuggestResult[]) => void) => {
-//         if (!suggest) {
-//             return;
-//         }
-//         const suggestions = suggest(text.trim(), 5);
-//         const data = suggestions
-//             .map(suggestion => {
-//                 const html = makeTagMatchHtml(suggestion, 'match');
-//                 return {
-//                     content: suggestion.tag.search,
-//                     description: `${html.cn}<dim> - ${html.en}</dim>`,
-//                 };
-//             });
+    readonly onInputChanged = async (text: string, suggestCb: (suggestResults: chrome.omnibox.SuggestResult[]) => void) => {
+        if (!suggest) {
+            return;
+        }
+        const suggestions = suggest(text.trim(), 5);
+        const data = suggestions
+            .map(suggestion => {
+                const html = makeTagMatchHtml(suggestion, 'match');
+                return {
+                    content: suggestion.tag.search,
+                    description: `${html.cn}<dim> - ${html.en}</dim>`,
+                };
+            });
 
-//         suggestCb(data);
-//     }
+        suggestCb(data);
+    }
 
-//     readonly onInputEntered = (text: string) => {
-//         chrome.tabs.create({
-//             url: `${this.origin}/?f_search=${encodeURIComponent(text)}`,
-//         });
-//     }
-// }
+    readonly onInputEntered = (text: string) => {
+        chrome.tabs.create({
+            url: `${this.origin}/?f_search=${encodeURIComponent(text)}`,
+        });
+    }
+}
 
-// let instance: OmniBox;
-// function init(): boolean {
-//     if (!chrome.omnibox) {
-//         return false;
-//     }
-//     instance = new OmniBox();
-//     return true;
-// }
+let instance: OmniBox;
+function init(): boolean {
+    if (!chrome.omnibox) {
+        return false;
+    }
+    instance = new OmniBox();
+    return true;
+}
 
-// export const omnibox = init();
-export const omnibox = false;
+export const omnibox = init();
