e86b6af709fa3e39494a92d8bfd726bdceb84be3
OpportunityLiu <opportunity@live.in>
2020-02-29 03:02:08 +0800

    fix missing db file

--- src/background/tag-database.ts
+++ src/background/tag-database.ts
@@ -57,12 +57,14 @@ class TagDatabase {
         const dbUrl = chrome.runtime.getURL('assets/tag.db');
         const r = await fetch(dbUrl);
         const buf = await r.arrayBuffer();
-        this.update(buf, defaultReleaseLink, new Date(0));
+        this.update(buf, true, defaultReleaseLink, new Date(0));
     }
 
-    update(data: ArrayBuffer, releaseLink: string, updateTime: Date = new Date()): void {
+    update(data: ArrayBuffer, isGziped: boolean, releaseLink: string, updateTime: Date = new Date()): void {
         const timer = logger.time('构建数据');
-        const tagDB: EHTDatabase = JSON.parse(pako.ungzip(new Uint8Array(data), { to: 'string' }));
+        const tagDB: EHTDatabase = JSON.parse(isGziped
+            ? (pako.ungzip(new Uint8Array(data), { to: 'string' }))
+            : (new TextDecoder('utf-8').decode(data)));
         const sha = tagDB.head.sha;
         const tagReplace: TagReplace = {};
         const tagList: TagList = [];
--- src/background/updater.ts
+++ src/background/updater.ts
@@ -1,7 +1,7 @@
 import { BehaviorSubject } from 'rxjs';
 import { browser } from 'webextension-polyfill-ts';
 
-import { DownloadStatus, ReleaseCheckData } from '../interface';
+import { DownloadStatus, ReleaseCheckData, GithubRelease } from '../interface';
 import { badgeLoading } from '../tool/badge-loading';
 import { chromeMessage } from '../tool/chrome-message';
 import { logger } from '../tool/log';
@@ -46,7 +46,7 @@ class Updater {
         this.initDownloadStatus();
         try {
             const data = await this.download();
-            tagDatabase.update(data.db, data.release.html_url);
+            tagDatabase.update(data.content, data.filename.endsWith('.gz'), data.release.html_url);
 
             badgeLoading.set('OK', '#00C801');
             this.pushDownloadStatus({ run: true, info: '更新完成', progress: 100, complete: true });
@@ -90,9 +90,9 @@ class Updater {
 
         const { sha, releaseLink } = await browser.storage.local.get(['sha', 'releaseLink']);
         const githubDownloadUrl = 'https://api.github.com/repos/ehtagtranslation/Database/releases/latest';
-        const info = await (await fetch(githubDownloadUrl)).json();
+        const info = await (await fetch(githubDownloadUrl)).json() as GithubRelease;
 
-        if (!(info && info.target_commitish)) {
+        if (!info?.target_commitish) {
             return null;
         }
         this.lastCheckData.next({
@@ -106,7 +106,7 @@ class Updater {
         return this.lastCheckData.value;
     }
 
-    private download(): Promise<{ release: any, db: ArrayBuffer }> {
+    private download(): Promise<{ release: GithubRelease, filename: string, content: ArrayBuffer }> {
         return new Promise(async (resolve, reject) => {
             if (this.loadLock) {
                 reject('已经正在下载');
@@ -116,38 +116,43 @@ class Updater {
             badgeLoading.set('', '#4A90E2', 2);
             this.pushDownloadStatus({ run: true, info: '加载中' });
             const checkData = await this.checkVersion();
-            if (!(checkData && checkData.githubRelease && checkData.githubRelease.assets)) {
+            if (!checkData?.githubRelease?.assets) {
+                logger.debug('checkData', checkData);
                 reject(new Error('无法获取版本信息'));
                 this.loadLock = false;
                 return;
             }
             const info = checkData.githubRelease;
-            const asset = info.assets.find((i: any) => i.name === 'db.html.json.gz');
-            const url = asset && asset.browser_download_url || '';
+            const asset = info.assets.find(i => i.name === 'db.html.json.gz') ?? info.assets.find(i => i.name === 'db.html.json');
+            const url = asset?.browser_download_url;
             if (!url) {
+                logger.debug('assets', info.assets);
                 reject(new Error('无法获取下载地址'));
                 this.loadLock = false;
                 return;
             }
 
+            const timer = logger.time(`开始下载 ${url}`);
             const xhr = new XMLHttpRequest();
             xhr.open('GET', url);
             xhr.responseType = 'arraybuffer';
             xhr.onload = () => {
                 try {
-                    resolve({ release: info, db: xhr.response as ArrayBuffer });
+                    resolve({ release: info, filename: asset.name, content: xhr.response as ArrayBuffer });
                     this.pushDownloadStatus({ info: '下载完成', progress: 100 });
                     badgeLoading.set('100', '#4A90E2', 1);
                 } catch (e) {
                     reject(new Error('数据无法解析'));
                     badgeLoading.set('ERR', '#C80000');
                 } finally {
+                    timer.end();
                     this.loadLock = false;
                 }
             };
             xhr.onerror = (e) => {
                 this.loadLock = false;
                 badgeLoading.set('ERR', '#C80000');
+                timer.end();
                 reject(e);
             };
             xhr.onprogress = (event) => {
