b6bef31c4a1544d738f9c05580da305122f4b1cf
Opportunity <opportunity@live.in>
2020-07-30 16:35:41 +0800

    自适应颜色主题

--- src/web-ext/popup.ts
+++ src/web-ext/popup.ts
@@ -15,8 +15,20 @@ import { Container } from 'services';
             window.close();
         },
     });
+
     const current = await browser.tabs.query({ active: true });
-    if (current && current.length >= 1 && current[0].url && new URL(current[0].url).hostname.includes('exhentai')) {
+    const currentHost = (current && current.length >= 1 && current[0].url && new URL(current[0].url).hostname) || '';
+
+    if (currentHost.includes('exhentai')) {
         document.body.classList.add('ex');
+    } else if (currentHost.includes('e-hentai')) {
+        document.body.classList.add('eh');
+    } else if ('matchMedia' in window) {
+        const matchesDarkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        if (matchesDarkTheme) {
+            document.body.classList.add('ex');
+        } else {
+            document.body.classList.add('eh');
+        }
     }
 })().catch(console.error);
