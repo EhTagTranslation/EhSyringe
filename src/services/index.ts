00bd202e944d3427c422fbb0a2585ff779ecbcf5
Opportunity <opportunity@live.in>
2021-01-29 18:08:53 +0800

    update

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,13 +1,4 @@
 import { Container as ContainerStatic, ContainerInstance } from 'typedi';
-export {
-    Service,
-    Inject,
-    InjectMany,
-    Token,
-    ObjectType,
-    ServiceIdentifier,
-    ServiceMetadata,
-    ServiceOptions,
-} from 'typedi';
+export { Service, Inject, InjectMany, Token, ServiceIdentifier, ServiceMetadata, ServiceOptions } from 'typedi';
 export type Container = ContainerInstance;
 export const Container = ContainerStatic.of('default');
