0013049c1e2d032a19833f62e92750bf87cf45df
xioxin <5716100+xioxin@users.noreply.github.com>
2021-09-14 10:08:48 +0800

    修改错别字

--- src/services/ui-translation/data/archiver.ts
+++ src/services/ui-translation/data/archiver.ts
@@ -22,6 +22,6 @@ merge(/^\/archiver\.php/, undefined, {
     'cancel': '取消',
     'Locating archive server and preparing file for download...': '找到存档服务器并准备下载文件...',
     '(this can take several minutes)': '（这可能需要几分钟的时间）',
-    'Please wait...': '请稍后...',
+    'Please wait...': '请稍候...',
     'Close': '取消',
 });
