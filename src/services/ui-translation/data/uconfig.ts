da990c4c4d943af5e8385e5d054b7e145d0be3c0
Opportunity <opportunity@live.in>
2021-12-04 17:15:53 +0800

    fix: add translation

--- src/services/ui-translation/data/uconfig.ts
+++ src/services/ui-translation/data/uconfig.ts
@@ -83,8 +83,11 @@ merge(/^\/uconfig\.php/, undefined, {
     ' character': ' 角色',
     ' group': ' 社团',
     ' artist': ' 艺术家',
+    ' cosplayer': ' Coser',
     ' male': ' 男性',
     ' female': ' 女性',
+    ' mixed:': ' 混合',
+    ' other:': ' 其他',
 
     'Tag Filtering Threshold': '标签筛选阈值',
     'You can soft filter tags by adding them to ': '你可以通过将标签加入「',
