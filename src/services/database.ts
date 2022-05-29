1fb1d530e2f561ba4e99261cee75513a531baef5
OpportunityLiu <opportunity@live.in>
2022-05-29 17:14:39 +0800

    fix: add cdn

--- src/services/database.ts
+++ src/services/database.ts
@@ -20,10 +20,13 @@ export class Database {
             const sha = data.mirror;
             if (typeof sha != 'string') throw new Error();
             return [
+                `https://fastly.jsdelivr.net/gh/EhTagTranslation/DatabaseReleases@${sha}/db.html.json`,
                 `https://cdn.jsdelivr.net/gh/EhTagTranslation/DatabaseReleases@${sha}/db.html.json`,
-                `https://gitcdn.xyz/cdn/EhTagTranslation/DatabaseReleases/${sha}/db.html.json`,
-                `https://rawcdn.githack.com/EhTagTranslation/DatabaseReleases/${sha}/db.html.json`,
+                `https://originfastly.jsdelivr.net/gh/EhTagTranslation/DatabaseReleases@${sha}/db.html.json`,
+                `https://gcore.jsdelivr.net/gh/EhTagTranslation/DatabaseReleases@${sha}/db.html.json`,
                 `https://cdn.statically.io/gh/EhTagTranslation/DatabaseReleases/${sha}/db.html.json`,
+                `https://rawcdn.githack.com/EhTagTranslation/DatabaseReleases/${sha}/db.html.json`,
+                `https://gitcdn.xyz/cdn/EhTagTranslation/DatabaseReleases/${sha}/db.html.json`,
             ];
         } catch {
             throw new Error(`GitHub 发布数据无法解析，可能需要更新插件版本`);
