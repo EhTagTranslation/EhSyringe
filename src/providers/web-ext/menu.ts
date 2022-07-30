4a04e4d25b922f0090a4952e8cdb448bd153d747
OpportunityLiu <Opportunity@Live.in>
2022-07-31 02:33:24 +0800

    fix: avoid duplicete menu

--- src/providers/web-ext/menu.ts
+++ src/providers/web-ext/menu.ts
@@ -6,15 +6,14 @@ export function createMenu(info: Menu): void {
     if (!chrome.contextMenus) {
         return;
     }
-    const id = Math.random().toString(36).slice(2);
     browser.contextMenus.create({
-        id: id,
+        id: info.title,
         title: info.title,
         targetUrlPatterns: info.targetUrlPatterns,
         contexts: info.contexts,
     });
     browser.contextMenus.onClicked.addListener((data: Menus.OnClickData): void => {
-        if (data.menuItemId !== id) return;
+        if (data.menuItemId !== info.title) return;
 
         info.onclick({
             url: data.mediaType ? data.srcUrl : data.linkUrl,
