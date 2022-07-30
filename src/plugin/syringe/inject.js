e994056bae35e8df4f2f78aa8da3bd27e1361673
OpportunityLiu <Opportunity@Live.in>
2022-07-31 02:02:55 +0800

    feat: inject scripts

--- src/plugin/syringe/inject.js
+++
@@ -1,6 +0,0 @@
-function toggle_advsearch_pane(b) {
-    document.getElementById('advdiv').style.display == 'none' ? show_advsearch_pane(b) : hide_advsearch_pane(b);
-}
-function toggle_filesearch_pane(b) {
-    document.getElementById('fsdiv').style.display == 'none' ? show_filesearch_pane(b) : hide_filesearch_pane(b);
-}
