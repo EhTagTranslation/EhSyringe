07ab716a04f30ffb61105acd010b60d912f018b9
OpportunityLiu <Opportunity@Live.in>
2022-07-31 02:25:23 +0800

    feat: 不需要设置 __webpack_public_path__

--- src/types/define.d.ts
+++ src/types/define.d.ts
@@ -1,2 +1,1 @@
 declare const __type: 'user-script' | 'web-ext';
-declare let __webpack_public_path__: string;
--- src/web-ext/polyfills.ts
+++ src/web-ext/polyfills.ts
@@ -3,4 +3,3 @@ import '../polyfills';
 import * as browser from 'webextension-polyfill';
 
 Object.defineProperty(globalThis, 'browser', { value: browser });
-__webpack_public_path__ = chrome.runtime.getURL('/');
