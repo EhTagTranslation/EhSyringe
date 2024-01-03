82884831f73023bbc9101f7c2eed01552be09f23
小白-白 <i@moedog.org>
2024-01-03 19:02:11 +0800

    Add translation

--- src/services/ui-translation/data/tools.ts
+++ src/services/ui-translation/data/tools.ts
@@ -107,3 +107,50 @@ merge(
         [/^Showing the last (\d+) of (\d+) recent tags:$/, '最近 $2 个标签中的最后 $1 个：'],
     ],
 );
+
+merge(
+    /^\/tools\/tagapprove/,
+    'repo.e-hentai.org',
+    {
+        'Pending Tags': '待审核标签',
+        'Approved Tags': '已批准标签',
+        'Rejected Tags': '已拒绝标签',
+
+        namespace: '命名空间',
+        gallery: '图库',
+        creator: '创建者',
+        'time created': '创建时间',
+        'time updated': '更新时间',
+        '(missing)': '(缺失)',
+        'Show Older >': '显示更早的 >',
+
+        'Tag Name:': '标签名称：',
+        'Namespace:': '命名空间：',
+        'Tag Group:': '标签组：',
+        'Created:': '创建时间：',
+        'revision requested': '需要修改',
+        'Updated:': '更新时间：',
+        'First Tag:': '首次标记：',
+        'Tagged On:': '标记次数：',
+        'Sources:': '来源：',
+        'Pending Moderator Approval': '待 MOD 审核',
+        REJECTED: '已拒绝',
+        APPROVED: '已批准',
+        ' by ': ' 由 ',
+
+        Comments: '评论',
+        'There are currently no comments for this request': '当前没有评论',
+        'If you have any (useful) information about this tag, you can submit it here.': '如果您有任何 (有用的) 关于此标签的信息，可以在此提交。',
+        'Submit Comment': '提交评论',
+        'Back to list': '返回列表',
+
+        '(flagged for revision)': '(标记为需要修改)',
+    },
+    [
+        [/^Tag Request #(\d+)$/, '标签申请 #$1'],
+        [/ \(exists in $/, ' (已存在于 '],
+        [/^(\d+) public galler(y|ies)$/, '$1 个公开图库'],
+        [/^ on (\d\d\d\d-\d\d-\d\d \d\d:\d\d)$/, ' 于 $1'],
+        [/^(\d\d\d\d-\d\d-\d\d \d\d:\d\d) by $/, '$1 由 '],
+    ],
+);
