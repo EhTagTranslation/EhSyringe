04e03b7b651e9e798feeb4498e832de1fa785b8b
OpportunityLiu <opportunity@live.in>
2024-11-26 20:43:40 +0800

    feat: Add translation

--- src/services/ui-translation/data/hentaiathome.ts
+++ src/services/ui-translation/data/hentaiathome.ts
@@ -181,9 +181,7 @@ merge(
 
         'Monthly Data Transfer Target': '每月数据流量限制',
         Unlimited: '无限',
-        ' GB per month': ' GB 每月',
-        'If you have a monthly data cap, you can provide it here. If the system detects that you are likely to exceed this target, it will reduce the priority of ranges on your client to reduce the load, but it is not guaranteed to stay below this value. Leave at 0 to limit with max speed alone. Must be at least 1000 GB if set.':
-            '如果你有每月数据限额，请在此设置。如果系统检测到你可能超过此限额，它将会降低客户端上的静态范围的优先度，以降低网络负载，但并不能保证低于设定值。保留为 0 表示仅限制最大速度。最低限制为 1000 GB。',
+        ' GB per month': ' GB/月',
         'Static Range allocation is limited to 1 per 5 GB. Clients cannot be assigned high-capacity ranges if set below 5 TB/month.':
             '每 1 组静态范围至少 5 GB。如果设置低于 5 TB/月，则不会被分配到大容量范围。',
         'To reduce the monthly data transfer target for this client below this level, you must first shut it down, then check the "Remove static ranges if necessary" option.':
@@ -247,7 +245,12 @@ merge(
             /^This client currently has ([\d.]+) static ranges assigned. Each requires at least 5 GB of monthly data transfer, so you cannot reduce it below ([\d.]+) GB without removing ranges.$/,
             '此客户端已分配 $1 组静态范围。每组需要至少 5 GB 的每月数据流量，所以您无法减少至低于 $2 GB，除非移除静态范围。',
         ],
+        [
+            /^If you have a monthly data cap, you can provide it here. If the system detects that you are likely to exceed this target, it will reduce the priority of ranges on your client to reduce the load, but it is not guaranteed to stay below this value. Leave at 0 to limit with max speed alone. Must be at least ([\d.]+) GB if set.$/,
+            '如果你有每月数据限额，请在此设置。如果系统检测到你可能超过此限额，它将会降低客户端上的静态范围的优先度，以降低网络负载，但并不能保证低于设定值。保留为 0 表示仅限制最大速度。最低限制为 $1 GB。',
+        ],
         [/^ static range\(s\) assigned: P(\d+) = $/, ' 组静态范围：P$1 = '],
+        [/^([\d.]+) GB per month$/, '$1 GB/月'],
         [/^([\d.]+) ([KMGTP]B)\/hour$/, '$1 $2/小时'],
         [/^([\d.]+) ([KMGTP]B)\/s$/, '$1 $2/s'],
     ],
